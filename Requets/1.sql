d2e7ca938a772463c02d3cf61618db27feb0fbe7
shift-lab-kuznetsov <e.kuznetsov1@g.nsu.ru>
2022-12-10 11:06:31 +0700

    1st task done

--- Requets/1.sql
+++ Requets/1.sql
@@ -1,4 +1,10 @@
-SELECT "Rent".client_id,
-    COUNT(*)
+SELECT C.id,
+       C.surname,
+       C.name,
+       C.fathername
+FROM     (SELECT "Rent".client_id,
+    COUNT(*) as rentedCount
 FROM "Rent"
-GROUP BY "Rent".client_id
+GROUP BY "Rent".client_id) ClientToRentCount,
+    "Clients" as C
+WHERE C.id = ClientToRentCount.client_id AND ClientToRentCount.rentedCount > :X
